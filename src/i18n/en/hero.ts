df56cc530489b8de0ab9900865d09a67f9d69da9
Madushan404 <madushanpradeep001@gmail.com>
2025-05-16 09:46:49 +0530

    customize landing page

--- src/i18n/en/hero.ts
+++ src/i18n/en/hero.ts
@@ -1,10 +1,11 @@
 export const Hore = {
-    'hero.heading': "Empower your team with",
-    'hero.headingSpan': "Simple task management",
+    'hero.heading': "Simplify Tasks,",
+    'hero.headingSpan': "Amplify Results",
     'hero.headingParagraph': "The only all-in-one open source solution that streamlines your business's success with easy navigation",
-    'hero.linksignupText': "Get Started for Free",
+    'hero.linksignupText': "Started for Free",
     'hero.imageAlt': "Worklenz - Simplify, Organize, Achieve:  Your Work, Elevated | Product Hunt",
     'hero.reviewsText': "20+ Product Hunt Reviews",
-    'hero.dashboardImgAlt': "Worklenz Dashboard"
+    'hero.dashboardImgAlt': "Worklenz Dashboard",
+    'hero.learnmoreText':"Learn more",
     
 }
